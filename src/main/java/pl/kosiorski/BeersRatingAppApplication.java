6b8867ef1062d988be6d4556f11a5de8dcdc538e
k0sa <kosiorski.tomasz@gmail.com>
2019-01-19 13:44:23 +0100

    first commit.

---
+++ src/main/java/pl/kosiorski/BeersRatingAppApplication.java
@@ -0,0 +1,14 @@
+package pl.kosiorski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeersRatingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeersRatingAppApplication.class, args);
+    }
+
+}
+
